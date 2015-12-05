b24aac7b0b66f6c75353f04f9cc8588a53412c92
zzk936409886 <936409886@qq.com>
2015-12-05 18:50:17 +0800

    clock

--- Clock/ViewController.m
+++ Clock/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import "zkview.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *clock;
+@property (weak, nonatomic) IBOutlet UIImageView *clockPoint;
+@property (weak, nonatomic) IBOutlet UIImageView *rotatePoint;
+@property (weak, nonatomic) IBOutlet UISlider *slider;
+@property (weak, nonatomic) IBOutlet zkview *myview;
 
 @end
 
@@ -16,7 +22,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.slider.minimumValue = 0;
+    self.slider.maximumValue = M_PI * 2;
+    NSLog(@"point (%.0f %.0f)",self.rotatePoint.layer.anchorPoint.x,self.rotatePoint.layer.anchorPoint.y);
+    self.rotatePoint.layer.anchorPoint = CGPointMake(0.5, 1);
+}
+
+- (IBAction)ratate:(UISlider*)sender {
+    self.rotatePoint.transform = CGAffineTransformMakeRotation(sender.value);
+    self.myview.value = sender.value;
 }
 
 - (void)didReceiveMemoryWarning {
